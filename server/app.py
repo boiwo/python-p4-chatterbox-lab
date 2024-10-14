a2b7fd5889a20f8b567a3f6aebcb21e8c1273fe2
benard <benardboiwo703@gmail.com>
2024-10-14 11:06:46 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-@app.route('/')
-def home():
-    return "Welcome to the Chatterbox API!"
+
 CORS(app)
 migrate = Migrate(app, db)
 
